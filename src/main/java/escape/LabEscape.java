80014051f79aa6293015c7e521a940d213fd8427
Robert Baboi <robert.baboi@gmail.com>
2017-08-10 22:19:29 +0100

    Initial test for the algorithm with the pieces mocked

--- src/main/java/escape/LabEscape.java
+++ src/main/java/escape/LabEscape.java
@@ -3,13 +3,16 @@ package escape;
 import escape.exception.NoEscapeException;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.List;
+
 @Component
 public class LabEscape {
 
     private static final char WALL = 'O';
     private static final char FREE = ' ';
 
-//    compilation issue, will fix later
+    //    compilation issue, will fix later
 //    private static final char PATH = '•';
     private static final char PATH = '1';
 
@@ -19,15 +22,92 @@ public class LabEscape {
      *                  The escape point is always on the border (see README)
      * @param startX    Starting row number for the escape. 0 based.
      * @param startY    Starting column number for the escape. 0 based.
-     * @return          A char matrix with the same labyrinth and a path drawn from the starting point to the escape
-     * @throws          NoEscapeException when no path exists to the outside, from the selected starting point
+     * @return A char matrix with the same labyrinth and a path drawn from the starting point to the escape
+     * @throws NoEscapeException when no path exists to the outside, from the selected starting point
      */
     public char[][] drawPathForEscape(
             char[][] labyrinth,
             int startX,
             int startY) throws NoEscapeException {
 
-        // continuing
-        return new char[][]{};
+        char[][] newLabyrinth = moveToPoint(labyrinth, startX, startY);
+
+        if (isEscapePoint(newLabyrinth, startX, startY)) {
+            return newLabyrinth;
+        }
+
+        List<LabPoint> movePoints = findMovePoints(newLabyrinth, startX, startY);
+
+        for (LabPoint movePoint : movePoints) {
+
+            int x = movePoint.getX();
+            int y = movePoint.getY();
+
+            return drawPathForEscape(newLabyrinth, x, y);
+        }
+
+
+        throw new NoEscapeException();
+
     }
+
+    /**
+     * Should return true if the point represents a escape
+     *
+     * @param labyrinth The labyrinth model
+     * @param x         The x coordinate
+     * @param y         The y coordinate
+     * @return True if the point represents an escape, false otherwise
+     */
+    boolean isEscapePoint(char[][] labyrinth, int x, int y) {
+        return false;
+    }
+
+    /**
+     * This method should return the labyrinth with the coordinates marked as 'visited'
+     *
+     * @param labyrinth The labyrinth model
+     * @param x         The x coordinate
+     * @param y         The y coordinate
+     * @return A new structure with the value at position (x,y) masked as 'visited'
+     */
+    char[][] moveToPoint(char[][] labyrinth, int x, int y) {
+
+        return new char[0][];
+    }
+
+
+    /**
+     * This method should return all possible move points around the starting position
+     * The points are ordered from in a clockwise perspective
+     * The north point is the starting position, so the order is (NORTH, EST, SOUTH, WEST)
+     *
+     * @param labyrinth The labyrinth model
+     * @param startX    The x coordinate for the starting position
+     * @param startY    The y coordinate for the starting position
+     * @return A set of possible move points
+     */
+    List<LabPoint> findMovePoints(char[][] labyrinth, int startX, int startY) {
+
+        return Collections.emptyList();
+    }
+
+    public static class LabPoint {
+
+        private int x, y;
+
+        public LabPoint(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
+
 }
--- src/test/java/escape/LabEscapeTest.java
+++ src/test/java/escape/LabEscapeTest.java
@@ -1,9 +1,17 @@
 package escape;
 
 import escape.exception.NoEscapeException;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 public class LabEscapeTest {
 
@@ -11,34 +19,67 @@ public class LabEscapeTest {
 
     LabEscape labEscape = new LabEscape();
 
+    @Before
+    public void setUp() throws Exception {
+        labEscape = spy(LabEscape.class);
+    }
+
     @Test
     public void drawPathForEscape_found() throws NoEscapeException {
 
         char[][] expectedRoute = typeTransformer
-                .toCharMatrix("OOOOOOOOOO\n" +
-                        "\tO1111O111O\n" +
-                        "\tO1OO1O1O1O\n" +
-                        "\tO1 O1O1O1O\n" +
-                        "\tO OO111O11\n" +
-                        "\tO OOOOOOOO\n" +
-                        "\tO        O\n" +
-                        "\tOOOOOOOOOO");
+                .toCharMatrix(
+                                "OOOO\n" +
+                                "OXO \n" +
+                                "OXXX\n" +
+                                "OO O\n" +
+                                "OOOO");
 
         char[][] labyrinth = typeTransformer
-                .toCharMatrix("OOOOOOOOOO\n" +
-                        "\tO    O   O\n" +
-                        "\tO OO O O O\n" +
-                        "\tO  O O O O\n" +
-                        "\tO OO   O  \n" +
-                        "\tO OOOOOOOO\n" +
-                        "\tO        O\n" +
-                        "\tOOOOOOOOOO");
+                .toCharMatrix(
+                                "OOOO\n" +
+                                "O O \n" +
+                                "O   \n" +
+                                "OO O\n" +
+                                "OOOO");
 
-        int startX = 3, startY = 1;
+        int startX = 1, startY = 1;
+
+        char[][] step1 = doStep(labyrinth, startX, startY);
+        char[][] step2 = doStep(step1, 2, 1);
+        char[][] step3 = doStep(step2, 2, 2);
+        char[][] step4a = doStep(step3, 2, 3);
+        char[][] step4b = doStep(step3, 3, 2);
+
+        when(labEscape.moveToPoint(any(char[][].class), eq(startX), eq(startY))).thenReturn(step1);
+        when(labEscape.moveToPoint(any(char[][].class), eq(2), eq(1))).thenReturn(step2);
+        when(labEscape.moveToPoint(any(char[][].class), eq(2), eq(2))).thenReturn(step3);
+        when(labEscape.moveToPoint(any(char[][].class), eq(2), eq(3))).thenReturn(step4a);
+        when(labEscape.moveToPoint(any(char[][].class), eq(3), eq(2))).thenReturn(step4b);
+
+        when(labEscape.isEscapePoint(any(), eq(2), eq(3))).thenReturn(true);
+
+        when(labEscape.findMovePoints(any(char[][].class), eq(1), eq(1))).thenReturn(asList(new LabEscape.LabPoint(2, 1)));
+        when(labEscape.findMovePoints(any(char[][].class), eq(2), eq(1))).thenReturn(asList(new LabEscape.LabPoint(2, 2)));
+        when(labEscape.findMovePoints(any(char[][].class), eq(2), eq(2))).thenReturn(asList(new LabEscape.LabPoint(2, 3), new LabEscape.LabPoint(3, 2)));
 
         char[][] escapeRoute = labEscape.drawPathForEscape(labyrinth, startX, startY);
 
-        assertThat(escapeRoute).isEqualTo(expectedRoute);
+        assertThat(typeTransformer.toMultilineString(escapeRoute))
+                .isEqualTo(typeTransformer.toMultilineString(expectedRoute));
+    }
+
+    char[][] doStep(char[][] labyrinth, int x, int y) {
+
+        char[][] step = new char[labyrinth.length][labyrinth[0].length];
+
+        for (int i = 0; i < labyrinth.length; i++) {
+            step[i] = Arrays.copyOf(labyrinth[i], labyrinth[i].length);
+        }
+
+        step[x][y] = 'X';
+
+        return step;
     }
 
 }
